020bdc425ede97f144d38d8012d7010a1a1eb558
Mannuc-90 <manpreet.c1990@gmail.com>
2021-05-14 18:01:06 +0530

    Spring basics - code cleanUp --> removing tightly coupled code

--- spring-basics/src/main/java/com/tech2dev/spring/basics/springbasics/BinarySearchImpl.java
+++ spring-basics/src/main/java/com/tech2dev/spring/basics/springbasics/BinarySearchImpl.java
@@ -39,24 +39,6 @@ public class BinarySearchImpl {
 		System.out.println(sortAlgrithm);// To check the sort algorithm being used
 		sortedNumbers=sortAlgrithm.sort(numbers);
 
-
-		/**
-		 * Below is tightly coupled code
-		 */
-		//If sort algo needed is bubble sort
-		BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
-		sortedNumbers = bubbleSortAlgorithm.sort(numbers);
-		System.out.println("numbers sorted using bubbleSort : "+Arrays.toString(sortedNumbers));
-
-		//If sort algo needed is quick sort
-		QuickSortAlgorithm quickSortAlgorithm = new QuickSortAlgorithm();
-		sortedNumbers = quickSortAlgorithm.sort(numbers);
-		System.out.println("numbers sorted using quickSort : "+Arrays.toString(sortedNumbers));
-
-		//Note : We need to execute any 1 of above sort methods, but that cannot be decided at runtime
-		//So everytime we need to switch the sort algo to be used, we need to modify this SearchImpl class
-		//Hence it is tightly coupled code
-
 		//Implement search logic
 
 		return 3;
--- spring-basics/src/main/java/com/tech2dev/spring/basics/springbasics/SpringBasicsApplication.java
+++ spring-basics/src/main/java/com/tech2dev/spring/basics/springbasics/SpringBasicsApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBasicsApplication {
 
 	public static void main(String[] args) {
-		//Creating the object of binarySearchImpl class
-		/* BinarySearchImpl binarySearchImpl = new BinarySearchImpl(); */
-		
+			
 		/**
 		 * Creating the object of binarySearchImpl class using the Sort alogrithm object needed
 		 * Based on the need of algorithm needed, this initiazation can be changed 
